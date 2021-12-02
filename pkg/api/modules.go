3d559918c69a272bd7efd614f7bcad5855bdefb5
beebeeoii <b33b33o11@gmail.com>
2021-12-02 18:56:38 +0800

    code cleanup

--- pkg/api/modules.go
+++ pkg/api/modules.go
@@ -7,13 +7,13 @@ import (
 )
 
 // Module struct is the datapack for containing details about every module
-type module struct {
+type Module struct {
 	Name         string
 	ModuleCode   string
 	Id           string
 	CreatorName  string
 	CreatorEmail string
-	period       string
+	Period       string
 }
 
 // raw struct is the datapack for containing api raw data
@@ -27,8 +27,9 @@ type raw struct {
 
 const MODULE_URL_ENDPOINT = "https://luminus.nus.edu.sg/v2/api/module/?populate=Creator%2CtermDetail%2CisMandatory"
 
-func (req Request) GetModules() ([]module, error) {
-	var modules []module //Initialise slice to capture of modules
+func (req Request) GetModules() ([]Module, error) {
+
+	var modules []Module //Initialise slice to capture of modules
 
 	request, err := http.NewRequest("GET", req.Url, nil)
 
@@ -59,7 +60,7 @@ func (req Request) GetModules() ([]module, error) {
 		if _, ok := content["access"]; ok { // only modules that can be accessed will be placed in modules slice
 
 			termDetail := content["termDetail"].(map[string]interface{}) //getting inner map
-			newStruct := module{
+			newStruct := Module{
 				content["courseName"].(string),
 				content["name"].(string),
 				content["id"].(string),
