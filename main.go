bd4a28784f015c2525c2d59dac9cb4db9890b5f4
beebeeoii <b33b33o11@gmail.com>
2021-12-04 21:08:59 +0800

    ensure single instance

--- main.go
+++ main.go
@@ -6,23 +6,22 @@ import (
 	"github.com/beebeeoii/lominus/internal/app"
 	appLock "github.com/beebeeoii/lominus/internal/app/lock"
 	"github.com/beebeeoii/lominus/internal/ui"
-
 	"github.com/juju/fslock"
 )
 
 func main() {
-	lock := fslock.New(appLock.GetLockPath())
-	err := lock.TryLock()
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer lock.Unlock()
-
 	appInitErr := app.Init()
 	if appInitErr != nil {
 		log.Fatalln(appInitErr)
 	}
+
+	lock := fslock.New(appLock.GetLockPath())
+	lockErr := lock.TryLock()
+
+	if lockErr != nil {
+		log.Fatalln(lockErr)
+	}
+	defer lock.Unlock()
 
 	uiInitErr := ui.Init()
 	if uiInitErr != nil {
