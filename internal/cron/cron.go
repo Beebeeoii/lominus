e5ce497691dcd2f0d0c26f3f0b11200e140a3b0f
beebeeoii <b33b33o11@gmail.com>
2021-12-05 19:10:51 +0800

    implemented cron

--- internal/cron/cron.go
+++ internal/cron/cron.go
@@ -10,26 +10,57 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
+var mainScheduler *gocron.Scheduler
+var mainJob *gocron.Job
+
 func Init() error {
-	preferences, prefLoadErr := pref.LoadPreferences(appPref.GetPreferencesPath())
-	if prefLoadErr != nil {
-		return prefLoadErr
+	mainScheduler = gocron.NewScheduler(time.Local)
+
+	preferences, loadPrefErr := pref.LoadPreferences(appPref.GetPreferencesPath())
+	if loadPrefErr != nil {
+		return loadPrefErr
 	}
 
-	if preferences.Frequency == -1 {
+	job, err := createJob(preferences.Frequency)
+	if err != nil {
+		return err
+	}
+
+	mainJob = job
+	mainScheduler.StartAsync()
+
+	return nil
+}
+
+func UpdateFrequency(frequency int) error {
+	mainScheduler.Clear()
+
+	if frequency == -1 {
 		return nil
 	}
 
-	scheduler := gocron.NewScheduler(time.Local)
-	_, cronErr := scheduler.Every(1).Seconds().Do(func() {
-		log.Println(time.Local)
-	})
-
-	if cronErr != nil {
-		return cronErr
+	job, err := createJob(frequency)
+	if err != nil {
+		return err
 	}
 
-	scheduler.StartAsync()
+	mainJob = job
+	mainScheduler.StartAsync()
 
 	return nil
+}
+
+func GetNextRun() time.Time {
+	return mainJob.NextRun()
+}
+
+func GetLastRan() time.Time {
+	return mainJob.LastRun()
+}
+
+func createJob(frequency int) (*gocron.Job, error) {
+	return mainScheduler.Every(frequency).Seconds().Do(func() {
+		log.Println(GetLastRan())
+		log.Println(GetNextRun())
+	})
 }
