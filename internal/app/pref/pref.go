0fcba43f132d1c56342029f302251cbea0ead6f1
beebeeoii <b33b33o11@gmail.com>
2022-01-21 15:23:15 +0800

    removed unnecessary pkg files

--- internal/app/pref/pref.go
+++ internal/app/pref/pref.go
@@ -5,16 +5,36 @@ import (
 	"path/filepath"
 
 	appDir "github.com/beebeeoii/lominus/internal/app/dir"
+	"github.com/beebeeoii/lominus/internal/file"
 	"github.com/beebeeoii/lominus/internal/lominus"
 )
+
+const PREFERENCES_FILE_NAME = lominus.PREFERENCES_FILE_NAME
 
 // Preferences struct describes the data being stored in the user's preferences file.
 type Preferences struct {
 	Directory string
 	Frequency int
+	LogLevel  string
 }
 
 // GetJwtPath returns the file path to user's preferences.
 func GetPreferencesPath() string {
 	return filepath.Join(appDir.GetBaseDir(), lominus.PREFERENCES_FILE_NAME)
+}
+
+// SavePreferences saves the user's preferences data onto local storage.
+func SavePreferences(filePath string, preferences Preferences) error {
+	return file.EncodeStructToFile(filePath, preferences)
+}
+
+// LoadPreferences loads the user's preferences data from local storage.
+func LoadPreferences(filePath string) (Preferences, error) {
+	preferences := Preferences{}
+	if !file.Exists(filePath) {
+		return preferences, &file.FileNotFoundError{FileName: filePath}
+	}
+	err := file.DecodeStructFromFile(filePath, &preferences)
+
+	return preferences, err
 }
