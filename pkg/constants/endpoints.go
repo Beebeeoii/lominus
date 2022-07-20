15beb794d5528ec2e33078df550976a44273335f
jiawei <b33b33o11@gmail.com>
2022-07-20 21:13:50 +0800

    feat: updated endpoints

--- pkg/constants/endpoints.go
+++ pkg/constants/endpoints.go
@@ -3,6 +3,9 @@ package constants
 // Canvas Endpoints
 const (
 	CANVAS_MODULES_ENDPOINT = "https://canvas.nus.edu.sg/api/v1/courses"
+	CANVAS_FOLDERS_ENDPOINT = "https://canvas.nus.edu.sg/api/v1/courses/%s/folders"
+	CANVAS_FILES_ENDPOINT   = "https://canvas.nus.edu.sg/api/v1/folders/%s/files"
+	CANVAS_FILE_ENDPOINT    = "https://canvas.nus.edu.sg/api/v1/files/%s"
 )
 
 // Luminus Endpoints
