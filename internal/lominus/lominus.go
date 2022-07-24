555f52762a08b2d937bbd28138b7fbdc94dfb264
jiawei <b33b33o11@gmail.com>
2022-07-24 20:47:05 +0800

    chore: renamed creds.gob and pref.gob

--- internal/lominus/lominus.go
+++ internal/lominus/lominus.go
@@ -7,8 +7,8 @@ const APP_VERSION = "1.2.1"
 
 const LOCK_FILE_NAME = "lominus.lock"
 
-const CREDENTIALS_FILE_NAME = "creds.gob"
-const PREFERENCES_FILE_NAME = "pref.gob"
+const CREDENTIALS_FILE_NAME = "credentials.gob"
+const PREFERENCES_FILE_NAME = "preferences.gob"
 const TELEGRAM_FILE_NAME = "telegram.gob"
 const TOKENS_FILE_NAME = "tokens.gob"
 const GRADES_FILE_NAME = "grades.gob"
