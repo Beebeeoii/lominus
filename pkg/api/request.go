ee457f728b1456a5a442ba2faeb86078a86da94c
jiawei <b33b33o11@gmail.com>
2022-07-28 17:10:41 +0800

    chore: removed unnecessary code

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -80,26 +80,6 @@ const GET_METHOD = "GET"
 const CONTENT_TYPE_FORM = "application/x-www-form-urlencoded"
 const CONTENT_TYPE_JSON = "application/json; charset=UTF-8"
 
-// BuildModuleRequest builds and returns a ModuleRequest that can be used for Module related operations
-// such as retrieving all modules.
-func BuildModuleRequest() (ModuleRequest, error) {
-	jwtToken, jwtTokenErr := retrieveJwtToken()
-	if jwtTokenErr != nil {
-		return ModuleRequest{}, jwtTokenErr
-	}
-
-	return ModuleRequest{
-		Request: Request{
-			Url: interfaces.Url{
-				Url:      MODULE_URL_ENDPOINT,
-				Platform: constants.Luminus,
-			},
-			Token:     jwtToken,
-			UserAgent: USER_AGENT,
-		},
-	}, nil
-}
-
 // TODO Documentations
 func BuildModulesRequest(token string, platform constants.Platform) (ModulesRequest, error) {
 	var url string
@@ -169,6 +149,7 @@ func BuildFoldersRequest(token string, platform constants.Platform, builder inte
 	}, nil
 }
 
+// TODO Documentations
 func BuildFilesRequest(token string, platform constants.Platform, folder Folder) (FilesRequest, error) {
 	var url string
 
@@ -194,85 +175,6 @@ func BuildFilesRequest(token string, platform constants.Platform, folder Folder)
 		Folder: folder,
 	}, nil
 }
-
-// func BuildCanvasDocumentRequest(token string, builder interface{}, mode int) (DocumentRequest, error) {
-// 	var urlEndpoint string
-
-// 	switch mode {
-// 	case GET_ALL_FOLDERS:
-// 		_, isModule := builder.(Module)
-// 		_, isFolder := builder.(Folder)
-// 		if !isModule && !isFolder {
-// 			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using Module or Folder to have mode=GET_ALL_FOLDERS")
-// 		}
-// 		urlEndpoint = constants.CANVAS_FOLDERS_ENDPOINT
-// 	case GET_ALL_FILES:
-// 		_, isModule := builder.(Module)
-// 		_, isFolder := builder.(Folder)
-// 		if !isModule && !isFolder {
-// 			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using Module or Folder to have mode=GET_ALL_FILES")
-// 		}
-// 		urlEndpoint = constants.CANVAS_FILES_ENDPOINT
-// 	case DOWNLOAD_FILE:
-// 		_, isFile := builder.(File)
-// 		if !isFile {
-// 			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using File to download")
-// 		}
-// 		urlEndpoint = constants.CANVAS_FILE_ENDPOINT
-// 	default:
-// 		return DocumentRequest{}, errors.New("invalid mode: mode provided is invalid. Valid modes are GET_ALL_FOLDERS (0), GET_ALL_FILES (1), DOWNLOAD_FILE (2)")
-// 	}
-
-// 	switch builder := builder.(type) {
-// 	case Module:
-// 		return DocumentRequest{
-// 			Folder: Folder{
-// 				Id:           builder.Id,
-// 				Name:         builder.ModuleCode,
-// 				Downloadable: true,
-// 				Ancestors:    []string{},
-// 				HasSubFolder: true,
-// 			},
-// 			Request: Request{
-// 				Url: interfaces.Url{
-// 					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-// 					Platform: constants.Canvas,
-// 				},
-// 				Token:     token,
-// 				UserAgent: USER_AGENT,
-// 			},
-// 			Mode: mode,
-// 		}, nil
-// 	case Folder:
-// 		return DocumentRequest{
-// 			Folder: builder,
-// 			Request: Request{
-// 				Url: interfaces.Url{
-// 					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-// 					Platform: constants.Canvas,
-// 				},
-// 				Token:     token,
-// 				UserAgent: USER_AGENT,
-// 			},
-// 			Mode: mode,
-// 		}, nil
-// 	case File:
-// 		return DocumentRequest{
-// 			File: builder,
-// 			Request: Request{
-// 				Url: interfaces.Url{
-// 					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-// 					Platform: constants.Canvas,
-// 				},
-// 				Token:     token,
-// 				UserAgent: USER_AGENT,
-// 			},
-// 			Mode: mode,
-// 		}, nil
-// 	default:
-// 		return DocumentRequest{}, errors.New("invalid builder: DocumentRequest must be built using Module, Folder or File only")
-// 	}
-// }
 
 // BuildGradeRequest builds and returns a GradeRequest that can be used for Grade related operations
 // such as retrieving grades of a module.
@@ -338,101 +240,6 @@ func BuildMultimediaVideoRequest(multimediaChannel MultimediaChannel) (Multimedi
 			UserAgent: USER_AGENT,
 		},
 	}, nil
-}
-
-// BuildDocumentRequest builds and returns a DocumentRequest that can be used for File/Folder related operations
-// such as retrieving files/folders of a module.
-// DocumentRequests must be built using Module/Folder/File only.
-// Building it with Folder enables you to specify the specific folder you are interested in.
-// Building it with Module taks you to the root folder of the module files.
-// Building it with File enables you to download the file.
-//
-// Modes available:
-// GET_FOLDERS - retrieves folders in a specific folder (root folder if Module was used to build the DocumentRequest). Nested folders are not returned.
-// GET_ALL_FILES - retrieves all files in a specific folder (root folder if Module was used to build the DocumentRequest). Nested files are returned.
-// DOWNLOAD_FILE - downloads a particular file. DocumentRequest must be built with File.
-func BuildDocumentRequest(builder interface{}, mode int) (DocumentRequest, error) {
-	jwtToken, jwtTokenErr := retrieveJwtToken()
-	if jwtTokenErr != nil {
-		return DocumentRequest{}, jwtTokenErr
-	}
-
-	var urlEndpoint string
-
-	switch mode {
-	case GET_ALL_FOLDERS:
-		_, isModule := builder.(Module)
-		_, isFolder := builder.(Folder)
-		if !isModule && !isFolder {
-			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using Module or Folder to have mode=GET_ALL_FOLDERS")
-		}
-		urlEndpoint = FOLDER_URL_ENDPOINT
-	case GET_ALL_FILES:
-		_, isModule := builder.(Module)
-		_, isFolder := builder.(Folder)
-		if !isModule && !isFolder {
-			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using Module or Folder to have mode=GET_ALL_FILES")
-		}
-		urlEndpoint = FILE_URL_ENDPOINT
-	case DOWNLOAD_FILE:
-		_, isFile := builder.(File)
-		if !isFile {
-			return DocumentRequest{}, errors.New("invalid mode: DocumentRequest must be built using File to download")
-		}
-		urlEndpoint = DOWNLOAD_URL_ENDPOINT
-	default:
-		return DocumentRequest{}, errors.New("invalid mode: mode provided is invalid. Valid modes are GET_ALL_FOLDERS (0), GET_ALL_FILES (1), DOWNLOAD_FILE (2)")
-	}
-
-	switch builder := builder.(type) {
-	case Module:
-		return DocumentRequest{
-			Folder: Folder{
-				Id:           builder.Id,
-				Name:         builder.ModuleCode,
-				Downloadable: true,
-				Ancestors:    []string{},
-				HasSubFolder: true,
-			},
-			Request: Request{
-				Url: interfaces.Url{
-					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-					Platform: constants.Luminus,
-				},
-				Token:     jwtToken,
-				UserAgent: USER_AGENT,
-			},
-			Mode: mode,
-		}, nil
-	case Folder:
-		return DocumentRequest{
-			Folder: builder,
-			Request: Request{
-				Url: interfaces.Url{
-					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-					Platform: constants.Luminus,
-				},
-				Token:     jwtToken,
-				UserAgent: USER_AGENT,
-			},
-			Mode: mode,
-		}, nil
-	case File:
-		return DocumentRequest{
-			File: builder,
-			Request: Request{
-				Url: interfaces.Url{
-					Url:      fmt.Sprintf(urlEndpoint, builder.Id),
-					Platform: constants.Luminus,
-				},
-				Token:     jwtToken,
-				UserAgent: USER_AGENT,
-			},
-			Mode: mode,
-		}, nil
-	default:
-		return DocumentRequest{}, errors.New("invalid builder: DocumentRequest must be built using Module, Folder or File only")
-	}
 }
 
 // retrieveJwtToken is a util function that loads user's JWT data to be used to communicate with Luminus servers.
