c0b201daa9f4cf71cd5a029d6bddb7686b4fa04b
jiawei <b33b33o11@gmail.com>
2022-07-29 13:46:17 +0800

    refactor: moved string constants to constants file

---
+++ internal/constants/ui.go
@@ -0,0 +1,26 @@
+package constants
+
+const (
+	// Credentials Tab
+	CREDENTIALS_TITLE = "Credentials"
+
+	LUMINUS_TAB_TITLE            = "Luminus"
+	LUMINUS_TAB_DESCRIPTION      = `Credentials are saved **locally**. It is **only** used for logging into [Luminus](https://luminus.nus.edu.sg).`
+	LUMINUS_USERNAME_TEXT        = "Username"
+	LUMINUS_USERNAME_PLACEHOLDER = "Eg: nusstu\\e0123456"
+	LUMINUS_PASSWORD_TEXT        = "Password"
+	LUMINUS_PASSWORD_PLACEHOLDER = "Password"
+
+	CANVAS_TAB_TITLE         = "Canvas"
+	CANVAS_TAB_DESCRIPTION   = `Token is saved **locally**. It is used to access [Canvas](https://canvas.nus.edu.sg/) **only**.`
+	CANVAS_TOKEN_TEXT        = "Canvas Token"
+	CANVAS_TOKEN_PLACEHOLDER = "Account > Settings > New access token > Generate Token"
+
+	SAVE_CREDENTIALS_TEXT           = "Save Credentials"
+	VERIFYING_MESSAGE               = "Please wait while we verify your credentials..."
+	VERIFICATION_SUCCESSFUL_MESSAGE = "Verification successful."
+	VERIFICATION_FAILED_MESSAGE     = "Verification failed. Please check your credentials."
+
+	// General
+	CANCEL_TEXT = "Cancel"
+)
--- internal/ui/credentials.go
+++ internal/ui/credentials.go
@@ -6,14 +6,11 @@ import (
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	appAuth "github.com/beebeeoii/lominus/internal/app/auth"
+	appConstants "github.com/beebeeoii/lominus/internal/constants"
 	"github.com/beebeeoii/lominus/internal/file"
 	logs "github.com/beebeeoii/lominus/internal/log"
 	"github.com/beebeeoii/lominus/internal/lominus"
 	"github.com/beebeeoii/lominus/pkg/auth"
-)
-
-const (
-	SAVE_CREDENTIALS_TEXT = "Save Credentials"
 )
 
 var credentialsPath string
@@ -22,7 +19,7 @@ var tokensPath string
 // getCredentialsTab builds the credentials tab in the main UI.
 func getCredentialsTab(parentWindow fyne.Window) (*container.TabItem, error) {
 	logs.Logger.Debugln("credentials tab loaded")
-	tab := container.NewTabItem("Credentials", container.NewVBox())
+	tab := container.NewTabItem(appConstants.CREDENTIALS_TITLE, container.NewVBox())
 
 	var credentials auth.CredentialsData
 	cPath, getCredentialsPathErr := appAuth.GetCredentialsPath()
@@ -63,33 +60,44 @@ func getCredentialsTab(parentWindow fyne.Window) (*container.TabItem, error) {
 	return tab, nil
 }
 
-func getLuminusSubTab(parentWindow fyne.Window, defaultCredentials auth.LuminusCredentials) (*container.TabItem, error) {
+func getLuminusSubTab(
+	parentWindow fyne.Window,
+	defaultCredentials auth.LuminusCredentials,
+) (*container.TabItem, error) {
 	logs.Logger.Debugln("luminus tab loaded")
-	tab := container.NewTabItem("Luminus", container.NewVBox())
+	tab := container.NewTabItem(appConstants.LUMINUS_TAB_TITLE, container.NewVBox())
 
-	description := widget.NewRichTextFromMarkdown("Credentials are saved **locally**. It is used to login to [Luminus](https://luminus.nus.edu.sg) **only**.")
+	description := widget.NewRichTextFromMarkdown(appConstants.LUMINUS_TAB_DESCRIPTION)
 	description.Wrapping = fyne.TextWrapWord
 
 	usernameEntry := widget.NewEntry()
-	usernameEntry.SetPlaceHolder("Eg: nusstu\\e0123456")
+	usernameEntry.SetPlaceHolder(appConstants.LUMINUS_USERNAME_PLACEHOLDER)
 	passwordEntry := widget.NewPasswordEntry()
-	passwordEntry.SetPlaceHolder("Password")
+	passwordEntry.SetPlaceHolder(appConstants.LUMINUS_PASSWORD_PLACEHOLDER)
 
 	usernameEntry.SetText(defaultCredentials.Username)
 	passwordEntry.SetText(defaultCredentials.Password)
 
-	luminusCredentialsForm := widget.NewForm(widget.NewFormItem("Username", usernameEntry), widget.NewFormItem("Password", passwordEntry))
+	luminusCredentialsForm := widget.NewForm(
+		widget.NewFormItem(appConstants.LUMINUS_USERNAME_TEXT, usernameEntry),
+		widget.NewFormItem(appConstants.LUMINUS_PASSWORD_TEXT, passwordEntry),
+	)
 
-	luminusSaveButton := widget.NewButton(SAVE_CREDENTIALS_TEXT, func() {
+	luminusSaveButton := widget.NewButton(appConstants.SAVE_CREDENTIALS_TEXT, func() {
 		luminusCredentials := auth.LuminusCredentials{
 			Username: usernameEntry.Text,
 			Password: passwordEntry.Text,
 		}
 
-		status := widget.NewLabel("Please wait while we verify your credentials...")
+		status := widget.NewLabel(appConstants.VERIFYING_MESSAGE)
 		progressBar := widget.NewProgressBarInfinite()
 
-		mainDialog := dialog.NewCustom(lominus.APP_NAME, "Cancel", container.NewVBox(status, progressBar), parentWindow)
+		mainDialog := dialog.NewCustom(
+			lominus.APP_NAME,
+			appConstants.CANCEL_TEXT,
+			container.NewVBox(status, progressBar),
+			parentWindow,
+		)
 		mainDialog.Show()
 
 		logs.Logger.Debugln("verifying credentials")
@@ -97,11 +105,19 @@ func getLuminusSubTab(parentWindow fyne.Window, defaultCredentials auth.LuminusC
 		mainDialog.Hide()
 		if err != nil {
 			logs.Logger.Debugln("verfication failed")
-			dialog.NewInformation(lominus.APP_NAME, "Verification failed. Please check your credentials.", parentWindow).Show()
+			dialog.NewInformation(
+				lominus.APP_NAME,
+				appConstants.VERIFICATION_FAILED_MESSAGE,
+				parentWindow,
+			).Show()
 		} else {
 			logs.Logger.Debugln("verfication succesful - saving credentials")
 			luminusCredentials.Save(credentialsPath)
-			dialog.NewInformation(lominus.APP_NAME, "Verification successful.", parentWindow).Show()
+			dialog.NewInformation(
+				lominus.APP_NAME,
+				appConstants.VERIFICATION_SUCCESSFUL_MESSAGE,
+				parentWindow,
+			).Show()
 		}
 	})
 
@@ -114,20 +130,25 @@ func getLuminusSubTab(parentWindow fyne.Window, defaultCredentials auth.LuminusC
 	return tab, nil
 }
 
-func getCanvasSubTab(parentWindow fyne.Window, defaultCredentials auth.CanvasCredentials) (*container.TabItem, error) {
+func getCanvasSubTab(
+	parentWindow fyne.Window,
+	defaultCredentials auth.CanvasCredentials,
+) (*container.TabItem, error) {
 	logs.Logger.Debugln("canvas tab loaded")
-	tab := container.NewTabItem("Canvas", container.NewVBox())
+	tab := container.NewTabItem(appConstants.CANVAS_TAB_TITLE, container.NewVBox())
 
-	description := widget.NewRichTextFromMarkdown("Token is saved **locally**. It is used to access [Canvas](https://canvas.nus.edu.sg/) **only**.")
+	description := widget.NewRichTextFromMarkdown(appConstants.CANVAS_TAB_DESCRIPTION)
 	description.Wrapping = fyne.TextWrapWord
 	canvasTokenEntry := widget.NewPasswordEntry()
-	canvasTokenEntry.SetPlaceHolder("Account > Settings > New access token > Generate Token")
+	canvasTokenEntry.SetPlaceHolder(appConstants.CANVAS_TOKEN_PLACEHOLDER)
 
 	canvasTokenEntry.SetText(defaultCredentials.CanvasApiToken)
 
-	canvasCredentialsForm := widget.NewForm(widget.NewFormItem("Canvas Token", canvasTokenEntry))
+	canvasCredentialsForm := widget.NewForm(
+		widget.NewFormItem(appConstants.CANVAS_TOKEN_TEXT, canvasTokenEntry),
+	)
 
-	canvasSaveButton := widget.NewButton(SAVE_CREDENTIALS_TEXT, func() {
+	canvasSaveButton := widget.NewButton(appConstants.SAVE_CREDENTIALS_TEXT, func() {
 		canvasCredentials := auth.CanvasCredentials{
 			CanvasApiToken: canvasTokenEntry.Text,
 		}
@@ -136,10 +157,15 @@ func getCanvasSubTab(parentWindow fyne.Window, defaultCredentials auth.CanvasCre
 			CanvasApiToken: canvasTokenEntry.Text,
 		}
 
-		status := widget.NewLabel("Please wait while we verify your credentials...")
+		status := widget.NewLabel(appConstants.VERIFYING_MESSAGE)
 		progressBar := widget.NewProgressBarInfinite()
 
-		mainDialog := dialog.NewCustom(lominus.APP_NAME, "Cancel", container.NewVBox(status, progressBar), parentWindow)
+		mainDialog := dialog.NewCustom(
+			lominus.APP_NAME,
+			appConstants.CANCEL_TEXT,
+			container.NewVBox(status, progressBar),
+			parentWindow,
+		)
 		mainDialog.Show()
 
 		logs.Logger.Debugln("verifying credentials")
@@ -147,12 +173,20 @@ func getCanvasSubTab(parentWindow fyne.Window, defaultCredentials auth.CanvasCre
 		mainDialog.Hide()
 		if err != nil {
 			logs.Logger.Debugln("verfication failed")
-			dialog.NewInformation(lominus.APP_NAME, "Verification failed. Please check your credentials.", parentWindow).Show()
+			dialog.NewInformation(
+				lominus.APP_NAME,
+				appConstants.VERIFICATION_FAILED_MESSAGE,
+				parentWindow,
+			).Show()
 		} else {
 			logs.Logger.Debugln("verfication succesful - saving credentials")
 			canvasCredentials.Save(credentialsPath)
 			canvasTokens.Save(tokensPath)
-			dialog.NewInformation(lominus.APP_NAME, "Verification successful.", parentWindow).Show()
+			dialog.NewInformation(
+				lominus.APP_NAME,
+				appConstants.VERIFICATION_SUCCESSFUL_MESSAGE,
+				parentWindow,
+			).Show()
 		}
 	})
 
