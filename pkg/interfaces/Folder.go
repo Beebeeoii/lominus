4b03669da84025e4718bebde19ff345625c34b60
jiawei <b33b33o11@gmail.com>
2022-08-07 23:28:15 +0800

    fix: files in root canvas folders not downloaded

--- pkg/interfaces/Folder.go
+++ pkg/interfaces/Folder.go
@@ -6,12 +6,13 @@ type FolderObject interface {
 
 // TODO Documentation
 type CanvasFolderObject struct {
-	Id            int    `json:"id"`
-	Name          string `json:"name"`
-	FullName      string `json:"full_name"`
-	HiddenForUser bool   `json:"hidden_for_user"`
-	FilesCount    int    `json:"files_count"`
-	FoldersCount  int    `json:"folders_count"`
+	Id             int    `json:"id"`
+	Name           string `json:"name"`
+	FullName       string `json:"full_name"`
+	HiddenForUser  bool   `json:"hidden_for_user"`
+	FilesCount     int    `json:"files_count"`
+	FoldersCount   int    `json:"folders_count"`
+	ParentFolderId int    `json:"parent_folder_id"`
 }
 
 type LuminusFolderObject struct {
