2acf7b3dbafbcfb86d1783540f74126415f1ccce
kevin chua <skullspine9@gmail.com>
2022-08-09 17:44:29 +0800

    fix: add formatting to Fatalln

--- internal/app/updater/updater.go
+++ internal/app/updater/updater.go
@@ -25,14 +25,14 @@ func DoSelfUpdate(parentWindow fyne.Window) {
 
 	latest, found, err := selfupdate.DetectLatest(GITHUB_REPO)
 	if err != nil {
-		logs.Logger.Fatalln("Error occurred while detecting version: %v", err)
+		logs.Logger.Fatalln(fmt.Sprintf("Error occurred while detecting version: %v", err))
 	}
 	if !found {
-		logs.Logger.Fatalln("Latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+		logs.Logger.Fatalln(fmt.Sprintf("Latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH))
 	}
 
 	if latest.LessOrEqual(VERSION) {
-		logs.Logger.Fatalln("Current version (%s) is the latest", VERSION)
+		logs.Logger.Fatalln(fmt.Sprintf("Current version (%s) is the latest", VERSION))
 	}
 
 	exe, err := os.Executable()
@@ -40,7 +40,7 @@ func DoSelfUpdate(parentWindow fyne.Window) {
 		logs.Logger.Fatalln("Could not locate executable path")
 	}
 	if err := selfupdate.UpdateTo(latest.AssetURL, latest.AssetName, exe); err != nil {
-		logs.Logger.Fatalln("Error occurred while updating binary: %v", err)
+		logs.Logger.Fatalln(fmt.Sprintf("Error occurred while updating binary: %v", err))
 	}
 
 	updateMessage := fmt.Sprintf(appConstants.UPDATE_DIALOG_MESSAGE, latest.Version())
