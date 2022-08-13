bb0da9dc6a656123c9c7a9e53c8862ae8083844c
jiawei <b33b33o11@gmail.com>
2022-08-14 01:52:56 +0800

    doc: pkg/interfaces

--- pkg/interfaces/Folder.go
+++ pkg/interfaces/Folder.go
@@ -1,3 +1,5 @@
+// Package interfaces provide the fundamental blueprint for how each object
+// looks like.
 package interfaces
 
 // CanvasFolderObject depicts the actual object return from Canvas.
@@ -16,6 +18,7 @@ type CanvasFolderObject struct {
 // LuminusFolderObject depicts the actual object return from Luminus.
 // There are more fields being returned by Luminus, but these are just the
 // relevant ones as of now.
+// For more details on what mapstructure means: https://github.com/mitchellh/mapstructure
 type LuminusFolderObject struct {
 	Id           string      `json:"id" `
 	Name         string      `json:"name"`
