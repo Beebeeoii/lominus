ed847921f2273f17b9b42971fb0d0764b058d4bf
jiawei <jiawei@jiaweilee.com>
2024-08-09 14:28:18 +0800

    chore: remove unused functions

--- internal/app/auth/auth.go
+++ internal/app/auth/auth.go
@@ -2,11 +2,7 @@
 package appAuth
 
 import (
-	"path/filepath"
-
 	"github.com/beebeeoii/lominus/internal/app"
-	appDir "github.com/beebeeoii/lominus/internal/app/dir"
-	appConstants "github.com/beebeeoii/lominus/internal/constants"
 	"github.com/boltdb/bolt"
 )
 
@@ -32,20 +28,6 @@ func GetCanvasCredentials() (CanvasCredentials, error) {
 	}
 
 	return canvasCredentials, nil
-}
-
-// GetJwtPath returns the file path to user's JWT data.
-func GetTokensPath() (string, error) {
-	var jwtPath string
-
-	baseDir, retrieveBaseDirErr := appDir.GetBaseDir()
-	if retrieveBaseDirErr != nil {
-		return jwtPath, retrieveBaseDirErr
-	}
-
-	jwtPath = filepath.Join(baseDir, appConstants.TOKENS_FILE_NAME)
-
-	return jwtPath, nil
 }
 
 // SaveCanvasCredentials saves the user's Canvas API token locally.
