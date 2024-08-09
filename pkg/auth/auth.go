3d0ede5a7ee12686e8766339fc16d21e02acae70
jiawei <jiawei@jiaweilee.com>
2024-08-09 14:19:26 +0800

    chore: remove unused functions

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -2,16 +2,8 @@
 package auth
 
 import (
-	appConstants "github.com/beebeeoii/lominus/internal/constants"
 	file "github.com/beebeeoii/lominus/internal/file"
 )
-
-// JsonResponse struct is the datapack for containing API authentication response raw data.
-type JsonResponse struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	ExpiresIn   int    `json:"expires_in"`
-}
 
 // CredentialsData struct is the datapack that contains all the credentials for
 // available LMS (Canvas etc.).
@@ -20,13 +12,6 @@ type JsonResponse struct {
 type CredentialsData struct {
 	CanvasCredentials CanvasCredentials
 }
-
-const CREDENTIALS_FILE_NAME = appConstants.CREDENTIALS_FILE_NAME
-
-const CONTENT_TYPE = "application/x-www-form-urlencoded"
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"
-const POST = "POST"
-const AUTH_METHOD = "FormsAuthentication"
 
 // saveCredentialsData saves the user's credentials data to local storage for future use.
 func saveCredentialsData(credentialsPath string, credentailsData CredentialsData) error {
@@ -59,12 +44,4 @@ func (t *CredentialsData) Merge(t2 CredentialsData) {
 	if t.CanvasCredentials == (CanvasCredentials{}) {
 		t.CanvasCredentials = t2.CanvasCredentials
 	}
-}
-
-// JwtExpiredError struct contains the JwtExpiredError which will be thrown when the JWT data has expired.
-type JwtExpiredError struct{}
-
-// JwtExpiredError error to be thrown when the JWT data has expired.
-func (e *JwtExpiredError) Error() string {
-	return "JwtExpiredError: JWT token has expired."
 }
